19857994790da849f65194b35ae2886e3a30af5f
fined000@naver.com <fined000@naver.com>
2023-07-04 23:50:16 +0900

    mdx start

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,6 +3,8 @@ import Date from "../../components/Date";
 import Layout from "../../components/Layout";
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import utilStyles from '../../styles/utils.module.css';
+import { MDXRemote } from "next-mdx-remote";
+import CodeBlock from "../../components/CodeBlock";
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -21,6 +23,14 @@ export async function getStaticProps({ params }) {
   };
 };
 
+const Button = ({children}) => {
+  return (
+    <button className="bg-black dark:bg-white bg-black text-lg text-teal-200 dark:text-teal-200 rounded-lg px-5" onClick={() => alert(`thanks to ${children}`)}>{children}</button>
+  )
+}
+
+const components = {Button, CodeBlock}
+
 export default function FirstPost({ postData }) {
   const router = useRouter();
 
@@ -37,7 +47,8 @@ export default function FirstPost({ postData }) {
             <Date dateString={postData.date} />
           </div>
           <br />
-          <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+          {postData.contentHtml && <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />}
+          {postData.mdxSource && <MDXRemote {...postData.mdxSource} components={components} />}
         </article>
       </Layout>
     </>
